735538a951ff18a8e75d8039b8aa9877ab7f3407
Gregorius Albert <galbert.djaja@gmail.com>
2022-10-05 23:08:13 +0700

    NURO-56 Parent Aktivitas Hari Ini - Page View Jumbotron ok Tableview in progress

--- Nuro/Components/Jumbotron/Jumbotron.swift
+++ Nuro/Components/Jumbotron/Jumbotron.swift
@@ -12,8 +12,8 @@ class Jumbotron: UIView {
     let imageView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleAspectFill
-        view.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
-        view.layer.borderWidth = 1
+//        view.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+//        view.layer.borderWidth = 1
         view.layer.cornerRadius = view.frame.height
         view.clipsToBounds = true
         return view
@@ -22,8 +22,18 @@ class Jumbotron: UIView {
     let clockLabel: UILabel = {
         let view = UILabel()
         view.textColor = .black
-        view.font = UIFont(name: view.font.fontName, size: 20)
-        view.text = "10.50"
+        view.font = UIFont.systemFont(ofSize: 80, weight: .bold)
+        view.text = "10.34"
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    let dateLabel: UILabel = {
+        let view = UILabel()
+        view.textColor = .black
+        view.font = UIFont.systemFont(ofSize: 32, weight: .regular)
+        view.text = "Senin, 26 September 2022"
+        view.backgroundColor = .orange
         return view
     }()
 
@@ -37,10 +47,12 @@ class Jumbotron: UIView {
     }
     
     func setup() {
+        self.layer.cornerRadius = 20
         backgroundColor = .yellow
         imageView.image = UIImage(named: "dummy")
         addSubview(imageView)
         addSubview(clockLabel)
+        addSubview(dateLabel)
         setupConstraints()
     }
     
@@ -52,8 +64,13 @@ class Jumbotron: UIView {
         }
         
         clockLabel.snp.makeConstraints { make in
-            make.top.bottom.equalTo(self).inset(35)
-            make.left.equalTo(<#T##other: ConstraintRelatableTarget##ConstraintRelatableTarget#>)
+            make.top.equalTo(self).inset(20)
+            make.left.equalTo(imageView.snp.right).offset(25)
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(clockLabel.snp.bottom).offset(20)
+            make.left.equalTo(imageView.snp.right).offset(25)
         }
         
     }
--- Nuro/Features/Parent/TodayActivity/Views/ParentTodayActivityView.swift
+++ Nuro/Features/Parent/TodayActivity/Views/ParentTodayActivityView.swift
@@ -14,18 +14,35 @@ class ParentTodayActivityView: UIView {
     let smallCapsuleButton = SmallCapsuleButton()
     let jumbotron = Jumbotron()
     
+    let headerLabel: UILabel = {
+        let view = UILabel()
+        view.textColor = .black
+        view.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        view.text = "Aktivitas Mendatang"
+        return view
+    }()
+    
+    let tableView: UITableView = {
+        let view = UITableView()
+        return view
+    }()
+    
     var delegate: ParentTodayActivityDelegate!
     var vc: ParentTodayActivityViewController!
     
     func setup(vc: ParentTodayActivityViewController) {
-        delegate = vc
         self.vc = vc
+        delegate = vc
+        tableView.dataSource = vc
+        tableView.delegate = vc
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         backgroundColor = .white
     
         setupNavigationBar()
         addSubview(jumbotron)
-        
+        addSubview(headerLabel)
+        addSubview(tableView)
         
         setupConstraints()
     }
@@ -54,7 +71,17 @@ class ParentTodayActivityView: UIView {
     private func setupConstraints() {
         jumbotron.snp.makeConstraints { make in
             make.top.left.right.equalTo(safeAreaLayoutGuide).inset(20)
-            make.height.equalTo(300)
+            make.height.equalTo(200)
+        }
+        
+        headerLabel.snp.makeConstraints { make in
+            make.top.equalTo(jumbotron.snp.bottom).offset(40)
+            make.left.equalTo(safeAreaLayoutGuide).inset(20)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(headerLabel.snp.bottom).offset(20)
+            make.left.right.bottom.equalTo(self).inset(0)
         }
     }
     
