5ae22c76008a592f3f1888f9121fa5bb719327cb
Gregorius Albert <galbert.djaja@gmail.com>
2022-10-10 20:57:29 +0700

    NURO-82 Setup font config and plist to be used on project

---
+++ Nuro/Utilities/Fonts.swift
@@ -0,0 +1,21 @@
+//
+//  Fonts.swift
+//  Nuro
+//
+//  Created by Gregorius Albert on 10/10/22.
+//
+
+import Foundation
+
+enum Fonts {
+    enum VisbyRoundCF {
+        static let bold = "VisbyRoundCF-Bold"
+        static let demiBold = "VisbyRoundCF-DemiBold"
+        static let extraBold = "VisbyRoundCF-ExtraBold"
+        static let extraLight = "VisbyRoundCF-ExtraLight"
+        static let heavy = "VisbyRoundCF-Heavy"
+        static let light = "VisbyRoundCF-Light"
+        static let medium = "VisbyRoundCF-Medium"
+        static let regular = "VisbyRoundCF-Regular"
+    }
+}
