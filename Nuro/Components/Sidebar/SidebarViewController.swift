04c9654ad0714cc5d09e6850d90f6122a9ed935a
Samuel Dennis <sdhasjim@gmail.com>
2022-10-10 18:47:53 +0700

    add file for sidebar component

---
+++ Nuro/Components/Sidebar/SidebarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SidebarViewController.swift
+//  Nuro
+//
+//  Created by Samuel Dennis on 10/10/22.
+//
+
+import UIKit
+
+class SidebarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Nuro/Components/Sidebar/TabBarItem.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarItem.swift
+//  Nuro
+//
+//  Created by Samuel Dennis on 10/10/22.
+//
+
+import Foundation
