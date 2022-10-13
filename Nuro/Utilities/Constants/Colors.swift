54f737315c98e6fd795c07c577a309b59d9c61c6
Gregorius Albert <galbert.djaja@gmail.com>
2022-10-13 12:28:05 +0700

    All colors ok

--- Nuro/Utilities/Constants/Colors.swift
+++ Nuro/Utilities/Constants/Colors.swift
@@ -23,7 +23,16 @@ enum Colors {
     }
     
     enum Text {
-        
+        static let onyx = ColorRGBA.fraction(r: 53, g: 58, b: 66)
+        static let verseGreen = ColorRGBA.fraction(r: 16, g: 128, b: 20)
+        static let blueViolet = ColorRGBA.fraction(r: 112, g: 96, b: 180)
+        static let gold = ColorRGBA.fraction(r: 177, g: 144, b: 22)
+        static let frostbite = ColorRGBA.fraction(r: 218, g: 40, b: 168)
+        static let blueSapphire = ColorRGBA.fraction(r: 23, g: 92, b: 143)
+        static let plum = ColorRGBA.fraction(r: 145, g: 71, b: 149)
+        static let russet = ColorRGBA.fraction(r: 139, g: 77, b: 31)
+        static let slateBlue = ColorRGBA.fraction(r: 101, g: 85, b: 199)
+        static let seaweed = ColorRGBA.fraction(r: 13, g: 121, b: 127)
     }
     
     enum Background {
@@ -42,17 +51,5 @@ enum Colors {
         static let pewterBlue = ColorRGBA.fraction(r: 235, g: 231, b: 252)
         static let brightGray = ColorRGBA.fraction(r: 239, g: 239, b: 239)
     }
-    
-//    static let royalPurple = UIColor(hex: "#7060B4FF")
-//    static let lavender = UIColor(hex: "#F5E1FFFF")
-//    static let butter = UIColor(hex: "#FFD97EFF")
-//    static let cream = UIColor(hex: "#FFFAEEFF")
-//    static let darkGreyBlue = UIColor(hex: "#353A42FF")
-//    static let surfieGreen = UIColor(hex: "#0D797FFF")
-//    static let pattensBlue = UIColor(hex: "#DBEFFEFF")
-//    static let matisse = UIColor(hex: "#175C8FFF")
-//
-//    static let white = UIColor(hex: "#FFFFFFFF")
-//    static let black = UIColor(hex: "#000000FF")
     
 }
