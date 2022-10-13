06891301a14c7175ecd95d6904ba8c1bab31680b
Gregorius Albert <galbert.djaja@gmail.com>
2022-10-13 12:14:02 +0700

    Color enums

--- Nuro/Utilities/Constants/Colors.swift
+++ Nuro/Utilities/Constants/Colors.swift
@@ -10,16 +10,36 @@ import UIKit
 
 enum Colors {
     
-    static let royalPurple = UIColor(hex: "#7060B4FF")
-    static let lavender = UIColor(hex: "#F5E1FFFF")
-    static let butter = UIColor(hex: "#FFD97EFF")
-    static let cream = UIColor(hex: "#FFFAEEFF")
-    static let darkGreyBlue = UIColor(hex: "#353A42FF")
-    static let surfieGreen = UIColor(hex: "#0D797FFF")
-    static let pattensBlue = UIColor(hex: "#DBEFFEFF")
-    static let matisse = UIColor(hex: "#175C8FFF")
-
-    static let white = UIColor(hex: "#FFFFFFFF")
-    static let black = UIColor(hex: "#000000FF")
+    enum Brand {
+        static let floralWhite = ColorRGBA.fraction(r: 255, g: 250, b: 238)
+    }
+    
+    enum Neutral {
+        
+    }
+    
+    enum Text {
+        
+    }
+    
+    enum Background {
+        
+    }
+    
+    enum Button {
+        
+    }
+    
+//    static let royalPurple = UIColor(hex: "#7060B4FF")
+//    static let lavender = UIColor(hex: "#F5E1FFFF")
+//    static let butter = UIColor(hex: "#FFD97EFF")
+//    static let cream = UIColor(hex: "#FFFAEEFF")
+//    static let darkGreyBlue = UIColor(hex: "#353A42FF")
+//    static let surfieGreen = UIColor(hex: "#0D797FFF")
+//    static let pattensBlue = UIColor(hex: "#DBEFFEFF")
+//    static let matisse = UIColor(hex: "#175C8FFF")
+//
+//    static let white = UIColor(hex: "#FFFFFFFF")
+//    static let black = UIColor(hex: "#000000FF")
     
 }
