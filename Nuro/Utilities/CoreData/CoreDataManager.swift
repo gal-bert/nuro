2fbc48884917d97f5ff2aad9d4beb11b01916805
Karen Natalia <karen.natalia888@gmail.com>
2022-10-17 09:58:35 +0700

    NURO-95 Revision - change to NSPersistentCloudKitContainer

--- Nuro/Utilities/CoreData/CoreDataManager.swift
+++ Nuro/Utilities/CoreData/CoreDataManager.swift
@@ -11,17 +11,21 @@ import CoreData
 class CoreDataManager {
     
     static let shared = CoreDataManager()
-    let container: NSPersistentContainer
+    let container: NSPersistentCloudKitContainer
     let context: NSManagedObjectContext
     
     private init() {
-        container = NSPersistentContainer(name: "Nuro")
+        container = NSPersistentCloudKitContainer(name: "Nuro")
         container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         context = container.viewContext
+        context.automaticallyMergesChangesFromParent = true
+        
+        // MARK: Check if chosen mergePolicy is correct after integration with CloudKit
+        context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
     }
     
 }
