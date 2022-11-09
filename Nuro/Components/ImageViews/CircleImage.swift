a341d76a843631c304736645c6ae59d23d1ea9fc
Gregorius Albert <galbert.djaja@gmail.com>
2022-11-09 11:13:33 +0700

    Changes by karennatalia

--- Nuro/Components/ImageViews/CircleImage.swift
+++ Nuro/Components/ImageViews/CircleImage.swift
@@ -9,13 +9,22 @@ import UIKit
 
 class CircleImage: UIImageView {
 
-    required init(size: CGFloat, imageName: String) {
+    init(size: CGFloat, imageName: String) {
         super.init(frame: .zero)
         
         self.image = UIImage(systemName: imageName)
         self.contentMode = .scaleAspectFit
         self.layer.cornerRadius = size / 2
         self.backgroundColor = Colors.Background.alabaster
+        self.clipsToBounds = true
+    }
+
+    init (size: CGFloat, image: UIImage) {
+        super.init(frame: .zero)
+
+        self.image = image
+        self.contentMode = .scaleAspectFit
+        self.layer.cornerRadius = size / 2
         self.clipsToBounds = true
     }
     
