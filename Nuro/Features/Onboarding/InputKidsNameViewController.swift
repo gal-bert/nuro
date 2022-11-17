69fc0f6e42490d070786d201dfa33d59afa24ea5
Samuel Dennis <sdhasjim@gmail.com>
2022-11-17 21:11:38 +0700

    NURO-229 On Boarding - Text field ketutup keyboard waktu ketik
    finish adjusting view when keyboard appear in Onboarding Page: InputParentName and InputKidsName

--- Nuro/Features/Onboarding/InputKidsNameViewController.swift
+++ Nuro/Features/Onboarding/InputKidsNameViewController.swift
@@ -120,6 +120,23 @@ class InputKidsNameViewController: UIViewController {
         setupConstraints()
 
         Transition.smoothAnimationPurple(subviews: view.subviews)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+           NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 
     @objc func skipAction() {
